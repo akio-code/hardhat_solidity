54da52afae0056b0e367e5a09c8981ecc853c700
lucas.akio <lucas.akio@7comm.com.br>
2023-06-26 23:16:36 -0300

    wip: added basic config

---
+++ hardhat.config.ts
@@ -0,0 +1,19 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    ganache: {
+      url: "http://127.0.0.1:7545",
+    }
+  },
+  paths: {
+    artifacts: "./src/artifacts",
+  }
+};
+
+export default config;
---
+++ scripts/deploy.ts
@@ -0,0 +1,31 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const unlockTime = currentTimestampInSeconds + 60;
+  //
+  // const lockedAmount = ethers.parseEther("0.001");
+
+  // const lock = await ethers.deployContract("Lock", [unlockTime], {
+  //   value: lockedAmount,
+  // });
+
+  const usdc = await ethers.deployContract("USDC");
+
+  await usdc.waitForDeployment();
+
+  // console.log(
+  //   `Lock with ${ethers.formatEther(
+  //     lockedAmount
+  //   )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  // );
+
+  console.log(`USDC  deployed to ${usdc.target}`)
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
