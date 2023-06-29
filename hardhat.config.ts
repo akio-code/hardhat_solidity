a3c5a2557ba08e36a7e2ae8400eec6598a444377
lucas.akio <lucas.akio@7comm.com.br>
2023-06-29 02:50:13 -0300

    refactor: added polygon deploy setup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,24 @@
+import dotenv from 'dotenv';
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   networks: {
-    hardhat: {
-      chainId: 1337,
-    },
+    hardhat: {},
     ganache: {
       url: "http://127.0.0.1:7545",
+      accounts: [process.env.GANACHE_PRIVATE_KEY!]
+    },
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY!]
     }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
   },
   paths: {
     artifacts: "./src/artifacts",
